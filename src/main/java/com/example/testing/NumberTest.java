eeeaf05b84b0160d62a5d8221d4afc7c2e0026eb
Thiru22-dnam <thirunavukkarasu16.samidurai>
2023-04-23 12:10:23 +0530

    Added Numbertest.java

---
+++ src/main/java/com/example/testing/NumberTest.java
@@ -0,0 +1,14 @@
+package com.example.testing;
+
+public class NumberTest {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=5;i++) {
+			
+			System.out.println(i);
+		}
+
+	}
+
+}
